776c4311538ba1be40e338d87de99958762f203a
Akhil Alex <akhilalex003z@gmail.com>
2024-12-23 20:13:35 +0530

    basic player movement script

---
+++ Player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta):
+
+	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = dir * SPEED
+
+	move_and_slide()
